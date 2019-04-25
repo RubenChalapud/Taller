5b3e3fd79f65f96f771c5e0b1f9f05fe221d5b1d
RubenChalapud <42219076+RubenChalapud@users.noreply.github.com>
2019-04-25 09:30:42 -0500

    1
    2

---
+++ src/modelo/film.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author admin-sala3
+ */
+public class film {
+    private int film_id;
+    private String title;
+    private String description;
+    private int release_year;
+    private int language_id;
+    private int priginal_language_id;
+    private double rental_rate;
+    private int length;
+    private double replacemet_cost;
+    private char rating;
+    private String special_features;
+    private 
+    
+}
